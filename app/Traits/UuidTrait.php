4d474da20da6633695a65c5a7a026d8b2ab179d7
tarip18 <taripinem18@gmail.com>
2024-02-29 16:10:18 +0700

    init: new repo for tokonline

---
+++ app/Traits/UuidTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Str;
+
+trait UuidTrait
+{
+    protected static function boot ()
+    {
+        // Boot other traits on the Model
+        parent::boot();
+        static::creating(function ($model) {
+            $model->incrementing = false;
+            $model->keyType = 'string';
+            $model->{$model->getKeyName()} = Str::uuid()->toString();
+        });
+    }
+
+    // Tells the database not to auto-increment this field
+    public function getIncrementing ()
+    {
+        return false;
+    }
+
+    // Helps the application specify the field type in the database
+    public function getKeyType ()
+    {
+        return 'string';
+    }
+}
